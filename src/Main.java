80d65d669536aa224137da262965271df216a995
devAlq <devAlq6@gmail.com>
2023-12-16 02:01:40 +0300

    abstract class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-          /* your code here */
+
+abstract class Vehicle{
+    protected String name;
+    protected String brand;
+    public Vehicle(String name, String brand){
+    this.name = name;
+    this.brand = brand;
+    }
+     public abstract void start();
    }
-}
+  
+  class Car extends Vehicle{
+   
+    public void start(){
+        System.out.println(name, brand);
+    
+  }}
+  public class Main {
+    public static void main(String[] args) {
+          Car obj = new Car("m5", "bmw");
+          obj.start();
+   }
+  }
+  
